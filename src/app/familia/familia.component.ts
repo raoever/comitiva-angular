0a4aa6f86b1ddb7c1c1e2c37b2f9ea76b90b1368
Rene <raoever@gmail.com>
2022-11-28 00:24:18 -0300

    Familia Componente Commit.

--- src/app/familia/familia.component.ts
+++ src/app/familia/familia.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {Observable, of} from "rxjs";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
+import {catchError} from "rxjs/operators";
+import {Router} from "@angular/router";
+import {Familia} from "../models/familia"
 
 @Component({
   selector: 'app-familia',
@@ -6,5 +12,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./familia.component.css']
 })
 export class FamiliaComponent {
+//   @ts-ignore
+  famsilias: Observable<Familia[]>;
+  displayedColumns = ['endereco', 'nomePai', 'nomeMae', 'acoes'];
 
+  isEditable: boolean = true;
+    // @ts-ignore
+    firstFormGroup: FormGroup;
+    // @ts-ignore
+    secondFormGroup: FormGroup;
+    // @ts-ignore
+//     @ViewChild('form') form;
+
+//      constructor(public dialog: MatDialog,
+//       private router: Router, private _formBuilder: FormBuilder) {
+//         this.candidatos = this.candidatoService.getCandidatos()
+//           .pipe(
+//             catchError(error => {
+//               console.log(error);
+//               return of([])
+//             })
+//           );
+//       }
+//
+//   ngOnInit(): void {
+//       this.firstFormGroup = this._formBuilder.group({
+//         tipo: [''],
+//       });
+//       this.secondFormGroup = this._formBuilder.group({
+//         parametro: [''],
+//       });
+//     }
+
+todasFamilias() {
+//     this.candidatos = this.candidatoService.getCandidatos();
+  }
 }
--- src/app/familia/familia.module.ts
+++ src/app/familia/familia.module.ts
@@ -2,13 +2,42 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FamiliaRoutingModule } from './familia-routing.module';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSortModule} from "@angular/material/sort";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatSelectModule} from "@angular/material/select";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FamiliaRoutingModule
+    FamiliaRoutingModule,
+    MatTableModule,
+        MatCardModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSortModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatMenuModule,
+        MatDialogModule,
+        MatStepperModule,
+        MatSelectModule
   ]
 })
 export class FamiliaModule { }
